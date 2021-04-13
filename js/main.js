b7e8ebb134455f8796cc2ab7b2d43f24c4064e2d
hipi <yuyehack@gmail.com>
2021-04-14 00:22:17 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,6 +19,10 @@ $(function () {
     };
   };
 
+  const isTouch = () => {
+    return navigator.maxTouchPoints > 0;
+  };
+
   const onMousemove = () => {
     const pageX = $(document).width();
     const pageY = $(document).height();
@@ -29,10 +33,28 @@ $(function () {
       const yAxis = (mouseY / pageY) * 100 - 50;
       $(".ghost-eyes").css("transform", `translate(${xAxis}%,${yAxis}%`);
     };
-    $(document).off("mousemove", handleMousemove);
-    $(document).on("mousemove", handleMousemove);
+    $(document).off("mousemove touchmove", handleMousemove);
+    $(document).on("mousemove touchmove", handleMousemove);
+  };
+
+  onTouchmove = () => {
+    const pageX = $(document).width();
+    const pageY = $(document).height();
+    const handleTouchmove = (event) => {
+      const mouseX = event.targetTouches[0].pageX;
+      const mouseY = event.targetTouches[0].pageY;
+      const xAxis = ((mouseX / pageX) * 100 - 50) / 2;
+      const yAxis = (mouseY / pageY) * 100 - 50;
+      $(".ghost-eyes").css("transform", `translate(${xAxis}%,${yAxis}%`);
+    };
+    $(document).off("touchmove", handleTouchmove);
+    $(document).on("touchmove", handleTouchmove);
   };
   onMousemove();
+  if (isTouch()) {
+    onTouchmove();
+  }
+
   $(window).on("rize", debounce(onMousemove, 500));
 
   //   今日诗词
