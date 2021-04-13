15d7cdca2d8c19a0a19ee7e84188656bbc7ca50c
hipi <yuyehack@gmail.com>
2021-04-13 23:49:45 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,46 @@
-function addCircle(x, y) {
-  var circle = document.createElement("div");
-  var animationTime = Math.round(Math.random() * 10);
-  circle.classList.add("circle");
-  circle.style.left = x + "vw";
-  circle.style.top = y + "vh";
-  circle.style.backgroundColor = randomColor();
-  circle.style.setProperty("--grow-time", `${animationTime}s`);
+$(function () {
+  const throttle = function (func, delay) {
+    let prev = Date.now();
+    return function () {
+      const context = this;
+      const args = arguments;
+      const now = Date.now();
+      if (now - prev >= delay) {
+        func.apply(context, args);
+        prev = Date.now();
+      }
+    };
+  };
+  const debounce = (fn, wait) => {
+    let timeout = null;
+    return function () {
+      if (timeout !== null) clearTimeout(timeout);
+      timeout = setTimeout(fn, wait);
+    };
+  };
 
-  requestAnimationFrame(() => {
-    document.body.appendChild(circle);
-    setTimeout(removeCircle.bind(this, circle), animationTime * 1000);
+  const onMousemove = () => {
+    const pageX = $(document).width();
+    const pageY = $(document).height();
+    const handleMousemove = (event) => {
+      const mouseX = event.pageX;
+      const mouseY = event.pageY;
+      const xAxis = ((mouseX / pageX) * 100 - 50) / 2;
+      const yAxis = (mouseY / pageY) * 100 - 50;
+      $(".ghost-eyes").css("transform", `translate(${xAxis}%,${yAxis}%`);
+    };
+    $(document).off("mousemove", handleMousemove);
+    $(document).on("mousemove", handleMousemove);
+  };
+  onMousemove();
+  $(window).on("rize", debounce(onMousemove, 500));
+
+  //   今日诗词
+  jinrishici.load(function (result) {
+    if (result.status === "success") {
+      const data = result.data;
+      const shiciStr = data.content.replace("。", "").split("，").join("<br>");
+      $("#shici").html(shiciStr);
+    }
   });
-}
-
-function removeCircle(circle) {
-  document.body.removeChild(circle);
-}
-
-function randomPosition() {
-  return [Math.random() * 95 + 1, Math.random() * 90 + 1];
-}
-
-function randomColor() {
-  const colors = [
-    "#81ecec",
-    "#74b9ff",
-    "#a29bfe",
-    "#ffeaa7",
-    "#fab1a0",
-    "#ff7675",
-    "#fd79a8",
-  ];
-  return colors[Math.round(Math.random() * colors.length)];
-}
-
-const timer = 350;
-
-setInterval(() => {
-  addCircle(...randomPosition());
-}, timer);
-
-window.addEventListener("load", () => {
-  document.querySelector(".bio").classList.add("ready");
 });
